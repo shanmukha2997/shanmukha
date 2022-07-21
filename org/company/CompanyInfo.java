3588841732941cdea883e88c85d94ee1e25f86a2
shanmukha2997 <shanmukharo2997@gmail.com>
2022-07-22 01:25:24 +0530

    new code added

---
+++ org/company/CompanyInfo.java
@@ -0,0 +1,25 @@
+package org.company;
+
+public class CompanyInfo {
+	public void companyName() {
+		System.out.println("company name is alstom");	
+	}
+	public void comanyId() {
+		System.out.println("company Id is 2997");
+	}
+	public void companyInfo() {
+		System.out.println("company Info is moblity is an nature");		
+	}
+	public void companyAddress() {
+		System.out.println("company Address is sricity");		
+	}
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+				c.companyName();
+		        c.comanyId();
+		        c.companyInfo();
+		        c.companyAddress();	
+		
+	}
+
+}
